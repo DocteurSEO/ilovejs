4f165120a29ff4ee8cc18d5e5131ac7f95c18be4
docteurseoo <docteurseoo@gmail.com>
2022-09-23 00:50:02 +0200

    add exo

---
+++ 00-create-h1/script.js
@@ -0,0 +1,56 @@
+/**
+Objective : learn to manipulate the DOM
+
+create an h1 tag in js, 
+add some text, 
+preferably a motivating quote. 
+the h1 text must be green . 
+ */
+
+// Your code here 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Validate your code 
+import {validation} from './validation.js'
+document.getElementById('validateexoahmed').addEventListener('click', validation)
---
+++ 02-Event/script.js
@@ -0,0 +1,58 @@
+/**
+Objective : learn to manipulate the DOM
+
+create a red square of 250px 
+id must be "sqr"
+
+Bonus
+on click the square becomes green
+with a black border of 5px  
+ */
+
+// Your code here 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Validate your code 
+import {validation} from './validation.js'
+document.getElementById('validateexoahmed').addEventListener('click', validation)
