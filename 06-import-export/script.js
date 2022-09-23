22d048e322bd745853753da76ad177b2c71f1e6e
docteurseoo <docteurseoo@gmail.com>
2022-09-23 08:07:52 +0200

    import export exo

---
+++ 06-import-export/script.js
@@ -0,0 +1,92 @@
+/**
+Objective : learn import export in js
+
+Export some functions
+ 
+ */
+
+
+/**
+addOne returns a number + 1
+ * @param {number} num -  .
+ * @return {number}  - num + 1
+*/
+
+ function addOne(num){
+
+     return null
+ }
+ 
+
+ /**
+ addTva returns taxes
+ * @param {number} ht -  .
+ * @return {number}  - TTC 
+*/
+
+ function addTva(ht){
+// tva à 20% 
+
+     return null
+ }
+
+
+ /**
+ addHTML add html to any tags in the document
+ * @param {string} selector -  select any dom element
+ * @param {string} htmlContent - add html  
+*/
+
+function addHTML(){
+
+        
+    }
+
+
+/**
+ changeColor change text color  to any tags in the document
+ * @param {string} selector -  select any dom element
+ * @param {string} color - add some color  
+*/
+
+const changeColor = function(){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Validate your code 
+import {validation} from './validation.js'
+document.getElementById('validateexoahmed').addEventListener('click', function(){
+    validation()
+})
